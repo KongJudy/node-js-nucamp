27a717d8b68a3b7ed1f9026c274b2a0c95310c91
Judy Kong <judy.kong817@gmail.com>
2023-06-03 11:30:44 -0400

    week 1 workshop

--- node-express/server.js
+++ node-express/server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
 const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
+// Create an instance express server
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
+// set up the routers
 app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 app.use(express.static(__dirname + '/public'));
 
